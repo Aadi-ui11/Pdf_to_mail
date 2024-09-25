2d10360eac3ec4f43c4fae9e1e6a86e594906dbc
Aadi-ui11 <Shushant.sharma501@gmail.com>
2024-09-25 12:43:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ def download_file(filename):
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
